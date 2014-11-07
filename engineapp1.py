b917397a7dda9cbcfda381c19b6b2f8f1c2de24d
unknown <T903544@D004678.corp.ads>
2014-11-07 14:53:52 -0500

    Redirect Test

--- engineapp1.py
+++ engineapp1.py
@@ -15,11 +15,12 @@
 # limitations under the License.
 #
 import webapp2
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello world After git!')
+        self.redirect ('http://concordia.ca')  
+        #self.response.write('Hello world After git!')
+        
 
 application = webapp2.WSGIApplication([
     ('/', MainPage)
