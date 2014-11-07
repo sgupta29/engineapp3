7e2f332b13c1a5d61ee851ddab79f38f263bf944
unknown <T903544@D004678.corp.ads>
2014-11-07 16:54:14 -0500

    testing deployment

--- engineapp1.py
+++ engineapp1.py
@@ -17,9 +17,8 @@
 import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.redirect ('http://concordia.ca')  
-        #self.response.write('Hello world After git!')
+        self.response.headers['Content-Type'] = 'text/json'
+        self.response.write('{"foo": "bar234"}')
         
 class Add(webapp2.RequestHandler):
     def get(self, x, y):
